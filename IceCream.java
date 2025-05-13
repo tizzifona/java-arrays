2f56f9744914441034f85e7df93fbee42c518ed4
tizzifona <tizzifona@gmail.com>
2025-05-13 03:17:28 +0200

    Added code for exercise IceCream

--- IceCream.java
+++ IceCream.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class IceCream {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numBalls;
+        List<String> flavors = new ArrayList<>();
+
+        System.out.print("¿Cuántas bolas de helado quieres? ");
+        numBalls = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+        System.out.println("¡Perfecto! Ahora escoge de qué sabor quieres cada helado: ");
+
+        for (int i = 0; i < numBalls; i++) {
+            System.out.print("    " + i + ". ");
+            String flavor = scanner.nextLine();
+            flavors.add(flavor);
+        }
+
+        System.out.println();
+        System.out.println("¡Gracias por tu orden! Has pedido los siguientes sabores:");
+        for (int i = 0; i < flavors.size(); i++) {
+            System.out.println("    " + i + ". " + flavors.get(i));
+        }
+
+        System.out.println();
+        System.out.println("¿Es correcto? presiona intro para continuar");
+        scanner.nextLine();
+
+        System.out.println("¡Listo, tenemos tu orden! Acércate al mostrador y Robotina te entregará tu helado");
+
+        scanner.close();
         
         //Vamos a hacer un programa que haga un pedido online por cada helado ordenado, vamos a escoger cuantas bolas de helado le pondremos y sus sabores.
 
